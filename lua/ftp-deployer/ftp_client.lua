3dfb54b68aaca777a2dbb3532adbf4f758866bb6
carloshmfs <carloshmfs.dev@pm.me>
2024-08-07 01:11:32 -0300

    create ftp client "class"

--- lua/ftp-deployer/ftp_client.lua
+++ lua/ftp-deployer/ftp_client.lua
@@ -1,14 +1,37 @@
 local FtpClient = {}
 
+local ftp = require("socket.ftp")
+
+local config = {
+    host = "ftp://test.rebex.net",
+    user = "",
+    password = "",
+    base_path = "",
+}
+
+local function open_connection(file)
+end
+
+function FtpClient:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
 function FtpClient:download(file)
+    -- local path = config.path .. "/" .. file
+
+    print(self, file)
+
+    -- local response = ftp.get({
+    --     host = config.host,
+    --     user = config.user,
+    --     password = config.password,
+    -- })
 end
 
-function FtpClient:upload(file)
-    local ftp = require("socket.ftp")
-    print(ftp.get("ftp://test.rebex.net/readme.txt"))
-end
-
-function FtpClient:open_connection(file)
+function FtpClient:upload(self, file)
 end
 
 return FtpClient
--- lua/ftp-deployer/init.lua
+++ lua/ftp-deployer/init.lua
@@ -5,7 +5,7 @@ function M:setup()
         desc = "descrição de teste",
     }
     vim.api.nvim_create_user_command("FtpUpload", function ()
-        local client = require("ftp-deployer.ftp_client")
+        local client = require("ftp-deployer.ftp_client").new()
         client.upload(vim.fn.expand("%:p"))
 
     end, command_opts)
