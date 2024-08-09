e66493ba3a2e51646704017fe84ca30230b75f7c
carloshmfs <carloshmfs.dev@pm.me>
2024-08-09 00:51:34 -0300

    feat: ftp download file from remote

--- lua/ftp-deployer/ftp_client.lua
+++ lua/ftp-deployer/ftp_client.lua
@@ -1,30 +1,34 @@
 local FtpClient = {}
 
 local ftp = require("socket.ftp")
+local ltn12 = require("ltn12")
 
 local config = {
-    host = "ftp.127.0.0.1",
+    host = "127.0.0.1",
     port = "2211",
     user = "robott",
     password = "",
-    base_remote_path = "",
+    base_remote_path = "/public_html/",
     base_local_path = "",
 }
 
 function FtpClient:download(file)
-    local path = config.base_remote_path .. file
+    local path = config.base_remote_path .. "test.php"
 
     print(path)
 
+    local t = {}
+
     local response, error = ftp.get({
         host = config.host,
+        sink = ltn12.sink.table(t),
         port = config.port,
         user = config.user,
         password = config.password,
         path = path
     })
 
-    print(response, error)
+    print(response, error, t[1])
 end
 
 function FtpClient:upload(self, file)
