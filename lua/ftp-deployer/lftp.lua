9565972a6b38ed71d14ac76de6a4ebea49d3e9b7
carloshmfs <carloshmfs.dev@pm.me>
2025-02-13 01:12:28 -0300

    feat(lftp): spawn lftp process

--- lua/ftp-deployer/lftp.lua
+++ lua/ftp-deployer/lftp.lua
@@ -3,10 +3,10 @@ local LFTP = {}
 LFTP.__index = LFTP
 
 local function make_cmd(cmd, host, user, password)
-    local lftp_cmd = "lftp -e 'set ssl:verify-certificate no; open ftp://" ..
-        user .. ":" .. password .. "@" .. host .. "; " .. cmd .. " '"
-
-    return lftp_cmd
+    return "lftp", {
+        "-e",
+        "'set ssl:verify-certificate no; open ftp://" .. user .. ":" .. password .. "@" .. host .. "; " .. cmd .. " '",
+    }
 end
 
 function LFTP.new(host, port, user, password)
@@ -22,8 +22,35 @@ function LFTP:put()
 end
 
 function LFTP:get(remote_file)
-    local cmd = make_cmd("cat " .. remote_file, self.host, self.user, self.password)
-    print(cmd)
+    local cmd, args   = make_cmd("cat " .. remote_file, self.host, self.user, self.password)
+    local stdin       = vim.uv.new_pipe()
+    local stdout      = vim.uv.new_pipe()
+    local stderr      = vim.uv.new_pipe()
+    local opts        = {
+        args = args,
+        stdio = { stdin, stdout, stderr },
+    }
+
+    local handle, pid = vim.uv.spawn(cmd, opts, function(code, signal)
+        -- print("exit code: " .. code .. " exit signal: " .. signal)
+        vim.notify("[FtpDeployer] an error ocurred while opening the lftp process", vim.log.levels.ERROR)
+    end)
+
+    vim.uv.read_start(stdout, function(err, data)
+        assert(not err, err)
+        if data then
+            -- print("stdout chunk", stdout, data)
+            return
+        end
+        -- print("stdout end", stdout)
+    end)
+
+    vim.uv.shutdown(stdin, function()
+        -- print("stdin shutdown", stdin)
+        vim.uv.close(handle, function()
+            -- print("process closed", handle, pid)
+        end)
+    end)
 end
 
 function LFTP:cat()
