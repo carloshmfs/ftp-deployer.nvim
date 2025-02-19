9ea39ad2efe85e7b67fbaeacfced1b3a31a144c5
carloshmfs <carloshmfs.dev@pm.me>
2025-02-19 01:04:23 -0300

    feat(lftp): spawn lftp process

--- lua/ftp-deployer/lftp.lua
+++ lua/ftp-deployer/lftp.lua
@@ -1,56 +1,56 @@
+---@class LFTP
+---@field host string
+---@field port string
+---@field user string
+---@field password string
 local LFTP = {}
 
 LFTP.__index = LFTP
 
+---@param cmd string
+---@param host string
+---@param user string
+---@param password table
 local function make_cmd(cmd, host, user, password)
-    return "lftp", {
+    return {
+        "lftp",
         "-e",
         "'set ssl:verify-certificate no; open ftp://" .. user .. ":" .. password .. "@" .. host .. "; " .. cmd .. " '",
     }
 end
 
 function LFTP.new(host, port, user, password)
-    local self = setmetatable({}, LFTP)
-    self.host = host
-    self.port = port
-    self.user = user
-    self.password = password
-    return self
+    return setmetatable({
+        host = host,
+        port = port,
+        user = user,
+        password = password
+    }, LFTP)
 end
 
 function LFTP:put()
 end
 
+---@param remote_file string
+---@return string
 function LFTP:get(remote_file)
-    local cmd, args   = make_cmd("cat " .. remote_file, self.host, self.user, self.password)
-    local stdin       = vim.uv.new_pipe()
-    local stdout      = vim.uv.new_pipe()
-    local stderr      = vim.uv.new_pipe()
-    local opts        = {
-        args = args,
-        stdio = { stdin, stdout, stderr },
+    local cmd     = make_cmd("cat " .. remote_file, self.host, self.user, self.password)
+    local opts    = {
+        -- text = true,
     }
-
-    local handle, pid = vim.uv.spawn(cmd, opts, function(code, signal)
-        -- print("exit code: " .. code .. " exit signal: " .. signal)
-        vim.notify("[FtpDeployer] an error ocurred while opening the lftp process", vim.log.levels.ERROR)
-    end)
-
-    vim.uv.read_start(stdout, function(err, data)
-        assert(not err, err)
-        if data then
-            -- print("stdout chunk", stdout, data)
+    local on_exit = function(obj)
+        if obj.code ~= 0 then
+            -- vim.notify("[FtpDeployer] Could not launch the lftp process.", vim.log.levels.ERROR)
+            print("[FtpDeployer] Could not launch the lftp process.")
             return
         end
-        -- print("stdout end", stdout)
-    end)
 
-    vim.uv.shutdown(stdin, function()
-        -- print("stdin shutdown", stdin)
-        vim.uv.close(handle, function()
-            -- print("process closed", handle, pid)
-        end)
-    end)
+        local file_contents = obj.stdout
+    end
+
+    vim.system(cmd, opts, on_exit)
+
+    return ""
 end
 
 function LFTP:cat()
